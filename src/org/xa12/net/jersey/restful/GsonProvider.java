2bf11ce3f2e0498752043fda3d06c8872693ebce
Leonhart_Lu@Dorm <leonhart811@gmail.com>
2016-11-02 22:03:39 +0800

    [Leonhart Lu] [MODIFY] 將GsonProvider之 java.io相關libraries逐一import

--- src/org/xa12/net/jersey/restful/GsonProvider.java
+++ src/org/xa12/net/jersey/restful/GsonProvider.java
@@ -12,7 +12,12 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
-import java.io.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
